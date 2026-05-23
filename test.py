2648adf4b04ece73e377634708d213b3eef3e527
Mrgt-0 <trita706@mail.ru>
2026-05-23 13:57:26 +0300

    m

--- test.py
+++
@@ -1,6 +0,0 @@
-with open('config.py', 'rb') as f:
-    content = f.read()
-    print("Содержимое файла в байтах:")
-    for i, b in enumerate(content):
-        if b > 127 or b < 32:
-            print(f"Позиция {i}: байт {hex(b)} (символ {repr(chr(b))})")
